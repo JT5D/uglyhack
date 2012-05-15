d8431e765dd0a322911f632f8aebf3ac80d14564
daniel.g.pettersson@gmail.com <daniel.g.pettersson@gmail.com@1e7fd97d-89db-86ba-5077-f0dc3e5da592>
2012-05-15 13:00:29 +0000

    prettier navbar, fade in/out nodes, show alert if no audio api found

--- webaudiotoy/js/main.js
+++ webaudiotoy/js/main.js
@@ -42,15 +42,26 @@ function doCreate(name) {
 }
 
 $(function() {
-	context = new (window.AudioContext || window.webkitAudioContext)();
 	
 	$('body').css('height', window.innerHeight - 40);
 	
-	//some kind fo bug makes audio analyze not kick in if creating destination node directly
-	setTimeout(function() {
-		nodes[0] = new DestinationNode(0);
-		nodes[1] = new SourceNode(1);
-		nodes[0].el.offset({left: window.innerWidth - 200, top: window.innerHeight/2-100});
-		nodes[1].el.offset({left: 30, top: window.innerHeight/2-100});
-	}, 1000);
+	try {
+		context = new (window.AudioContext || window.webkitAudioContext)();
+	} catch(e) {
+		context = null;
+	}
+	
+	if(context == null) {
+		$('#noWebAudioBox').modal();
+	} else {
+		//some kind fo bug makes audio analyze not kick in if creating destination node directly
+		setTimeout(function() {
+			nodes[0] = new DestinationNode(0);
+			nodes[1] = new SourceNode(1);
+			nodes[2] = new BiquadFilterNode(2);
+			nodes[0].el.offset({left: window.innerWidth - 200, top: window.innerHeight/2-150});
+			nodes[1].el.offset({left: 30, top: window.innerHeight/2-100});
+			nodes[2].el.offset({left: window.innerWidth/2-70, top: window.innerHeight/2-200});
+		}, 1000);
+	}
 }); 
