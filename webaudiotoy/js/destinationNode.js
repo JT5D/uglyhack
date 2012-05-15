e465fdb964d54bfa0b56d6e7bcdbf461a8975757
daniel.g.pettersson@gmail.com <daniel.g.pettersson@gmail.com@1e7fd97d-89db-86ba-5077-f0dc3e5da592>
2012-05-15 10:00:11 +0000

    just a little bit more layout

--- webaudiotoy/js/destinationNode.js
+++ webaudiotoy/js/destinationNode.js
@@ -1,29 +1,25 @@
 var DestinationNode = BaseNode.extend({
   	init: function(index){
   		this._super(index);
-  		this.thingy = context.createAnalyser();
-  		this.thingy.minDecibels = -70;
+  		this.thingy = null;//context.createAnalyser();
+  		//this.thingy.minDecibels = -70;
   		this.name = "destination";
   		this.createMainEl(false, true, false);
 		
-		var freqDataTags = new Array();
-		var analyzer = this.thingy;
-		var processor = context.createJavaScriptNode(2048, 1, 1);
-		this.thingy.connect(processor);
-	    processor.connect(context.destination);
+		//var analyzer = this.thingy;
+		//var processor = context.createJavaScriptNode(2048, 1, 1);
+		//this.thingy.connect(processor);
+	    //processor.connect(context.destination);
 
-	    processor.onaudioprocess = function(e) {
+	    //processor.onaudioprocess = function(e) {
 //		    var freqByteData = new Uint8Array(analyzer.frequencyBinCount);
 //		    analyzer.getByteFrequencyData(freqByteData);
-		};
+		//};
   	},
   	getConnections: function() {
 		var arr = new Array();
 		arr[0] = context.destination;
-		arr[1] = this.thingy;
+		//arr[1] = this.thingy;
 		return arr;
-	},
-	toString: function() {
-		return this.name;
 	}
 });
