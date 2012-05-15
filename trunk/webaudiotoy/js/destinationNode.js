1b8ab9eb021e48ecd7f549390849f7434fd3d0d4
daniel.g.pettersson@gmail.com <daniel.g.pettersson@gmail.com@1e7fd97d-89db-86ba-5077-f0dc3e5da592>
2012-05-15 15:26:43 +0000

    added canvas based visualizer to dest node.

--- trunk/webaudiotoy/js/destinationNode.js
+++ trunk/webaudiotoy/js/destinationNode.js
@@ -1,25 +1,29 @@
 var DestinationNode = BaseNode.extend({
   	init: function(index){
   		this._super(index);
-  		this.thingy = null;//context.createAnalyser();
+  		this.thingy = context.createAnalyser();
   		//this.thingy.minDecibels = -70;
   		this.name = "destination";
-  		this.createMainEl(false, true, false);
+  		var el = this.createMainEl(false, true, false);
+  		el.css('margin', 0);
 		
-		//var analyzer = this.thingy;
-		//var processor = context.createJavaScriptNode(2048, 1, 1);
-		//this.thingy.connect(processor);
-	    //processor.connect(context.destination);
+		var analyzer = this.thingy;
+		var processor = context.createJavaScriptNode(2048, 1, 1);
+		this.thingy.connect(processor);
+	    processor.connect(context.destination);
+	    
+	    var soundVisualizer = new SoundVisualizer(el, 150, 120);
 
-	    //processor.onaudioprocess = function(e) {
-//		    var freqByteData = new Uint8Array(analyzer.frequencyBinCount);
-//		    analyzer.getByteFrequencyData(freqByteData);
-		//};
+	    processor.onaudioprocess = function(e) {
+		    var freqByteData = new Uint8Array(analyzer.frequencyBinCount);
+		    analyzer.getByteFrequencyData(freqByteData);
+		    soundVisualizer.visualize(freqByteData);
+		};
   	},
   	getConnections: function() {
 		var arr = new Array();
 		arr[0] = context.destination;
-		//arr[1] = this.thingy;
+		arr[1] = this.thingy;
 		return arr;
 	}
 });
