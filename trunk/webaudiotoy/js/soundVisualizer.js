d8a1d4095ee8947f0c8422d781277fb7435090ee
daniel.g.pettersson@gmail.com <daniel.g.pettersson@gmail.com@1e7fd97d-89db-86ba-5077-f0dc3e5da592>
2012-05-16 07:22:15 +0000

    added more sound visualizer stuff, added loader overlay

--- trunk/webaudiotoy/js/soundVisualizer.js
+++ trunk/webaudiotoy/js/soundVisualizer.js
@@ -1,13 +1,13 @@
 var SoundVisualizer = Class.extend({
 	init: function(el, width, height){
-		var canvas = $('<canvas width="' + width + '" height="' + height + '">');
+		this.canvas = $('<canvas width="' + width + '" height="' + height + '">');
 		this.w = width;
 		this.h = height;
-		el.append(canvas);
-		this.ctx = canvas[0].getContext("2d");
+		el.append(this.canvas);
+		this.ctx = this.canvas[0].getContext("2d");
 	},
-	visualize: function(data) {
-		this.ctx.fillStyle = "rgba(255,255,255,0.06)";
+	visualizeFrequencyData: function(data) {
+		this.ctx.fillStyle = "rgba(255,255,255,0.1)";
 		this.ctx.fillRect(0,0,this.w,this.h);
 		this.ctx.fillStyle = "rgba(200,0,0,0.2)";
 		
@@ -18,6 +18,20 @@ var SoundVisualizer = Class.extend({
 			v = data[i*h];
 			s = (v/255)*this.h;
 			this.ctx.fillRect(i, (this.h-s), 1, s);
+		}
+	},
+	visualizeTimeDomainData: function(data) {
+		this.ctx.fillStyle = "rgba(255,255,255,0.1)";
+		this.ctx.fillRect(0,0,this.w,this.h);
+		this.ctx.fillStyle = "rgba(200,0,0,0.5)";
+		
+		var h = Math.floor(data.length / this.w);
+		var v = 0;
+		var s = 0;
+		for(var i = 0; i < this.w; i++) {
+			v = data[i*h];
+			s = (v/255)*this.h;
+			this.ctx.fillRect(i, s, 1, 1);
 		}
 	}
 });
