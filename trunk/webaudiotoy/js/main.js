27a0f985f971bc260e923ca773d44be5b9de7cce
daniel.g.pettersson@gmail.com <daniel.g.pettersson@gmail.com@1e7fd97d-89db-86ba-5077-f0dc3e5da592>
2012-05-25 12:31:26 +0000

    Finished save/load/share feature, except fine grained node data saving.

--- trunk/webaudiotoy/js/main.js
+++ trunk/webaudiotoy/js/main.js
@@ -27,11 +27,17 @@ var nodes = new Array();
 var context = null;
 
 $(function() {
+	
+	var populateLoadSelect = function() {
+		var loadSelect = $('#loadSelect');
+		loadSelect.empty();
+		var saves = new SaveHandler().getAllSavesInLocalStorage();
+		for(var i in saves) {
+			loadSelect.append($('<option>').html(saves[i]));
+		}
+	}
 
 	$('body').css('height', window.innerHeight - 40);
-	$('.itemCreator').draggable({
-		containment: 'parent'
-	})
 	$('a.brand').tooltip({placement: 'bottom'});
 	$('#firstTimeChk').on('change', function() {
 		if(this.checked) {
@@ -52,11 +58,16 @@ $(function() {
 	} else {
 		//some kind fo bug makes audio analyze not kick in if creating destination node directly
 		setTimeout(function() {
-			nodes[0] = new SourceNode(0);
-			nodes[1] = new DestinationNode(1);
-			
-			nodes[0].el.offset({left: 250, top: window.innerHeight/2-100});
-			nodes[1].el.offset({left: window.innerWidth - 200, top: window.innerHeight/2-150});
+			var p = getUrlParams();
+			if(p.data) {
+				new SaveHandler().loadSaveData(p.data);
+			} else {
+				nodes[0] = new SourceNode(0);
+				nodes[1] = new DestinationNode(1);
+				
+				nodes[0].el.offset({left: 250, top: window.innerHeight/2-100});
+				nodes[1].el.offset({left: window.innerWidth - 200, top: window.innerHeight/2-150});
+			}
 		}, 1000);
 
 		if(!localStorage["beenherebefore"]) {
@@ -65,107 +76,33 @@ $(function() {
 		}
 
 		$('#saveOkBtn').on('click', function() {
-
-			var save = {
-				name: $('#saveTxt').val(),
-				nodes: []
-			};
-
-			for(var i in nodes) {
-				var n = nodes[i];
-				if(!n.deleted) {
-					var conns = new Array();
-					for(var j in n.myConnections) {
-						if(!n.myConnections[j].deleted) {
-							conns.push(n.myConnections[j].idx);
-						}
-					}
-
-					save.nodes.push({
-						i: n.idx,
-						sn: n.shortName,
-						p: n.el.offset(),
-						c: conns
-					});
-				}
-			}
-
-			localStorage["save"] = window.btoa(JSON.stringify(save));
-
+			var saveName = $('#saveTxt').val();
+			new SaveHandler().saveToLocalStorage(saveName);
+			populateLoadSelect();
 			$('#saveBox').modal('hide');
 		});
 
-		$('#loadBtn').on('click', function() {
-			var data = localStorage["save"];
-			if(data) {
-				//load data
-				var save = JSON.parse(window.atob(data));
-
-				//clean up old nodes
-				for(var i in nodes) {
-					for(var j in nodes[i].myConnections) {
-						nodes[i].disconnectFrom(nodes[i].myConnections[j]);
-					}
-					nodes[i].shutdown();
-					nodes[i].el.remove();
-				}
-				nodes = new Array();
-
-				//create saved nodes
-				for(var i in save.nodes) {
-					var n = save.nodes[i];
-					var node = null;
-					switch(n.sn) {
-						case 'mn': node = new MicrophoneNode(n.i);	break;
-						case 'gn': node = new GainNode(n.i);	break;
-						case 'scn': node = new ScriptNode(n.i);	break;
-						case 'son': node = new SourceNode(n.i);	break;
-						case 'bfn': node = new BiquadFilterNode(n.i);	break;
-						case 'cn': node = new ConvolverNode(n.i);	break;
-						case 'deln': node = new DelayNode(n.i);	break;
-						case 'dstn': node = new DestinationNode(n.i);	break;
-						case 'dcn': node = new DynamicsCompressorNode(n.i);	break;
-						case 'wsn': node = new WaveShaperNode(n.i);	break;
-						case 'on': node = new OscillatorNode(n.i);	break;
-					}
-					node.el.offset(n.p);
-					nodes[n.i] = node;
-				}
-
-				//connect saved nodes
-				for(var i in save.nodes) {
-					var n = save.nodes[i];
-					if(n.c.length > 0) {
-						for(var j in n.c) {
-							var connectTo = n.c[j];
-							nodes[n.i].connectTo(nodes[connectTo]);
-						}
-					}
-				}
-
-				//create connection lines
-				for(var i in nodes) {
-					var fromN = nodes[i];
-					if(fromN) {
-						for(var j in fromN.myConnections) {
-							var toN = fromN.myConnections[j];
-							fromN.createConnectionLine(fromN.el,toN.el,fromN.idx,toN.idx, false);
-						}
-						fromN.updateConnectionLines();
-					}
-				}
-
-			} else {
-				alert('nothing saved');
-			}
+		$('#loadOkBtn').on('click', function() {
+			var saveName = $('#loadSelect').val();
+			new SaveHandler().loadFromLocalStorage(saveName);
+			$('#saveTxt').val(saveName);
+			$('#loadBox').modal('hide');
 		});
+		
+		$('#shareBtn').on('click', function() {
+			var shareUrl = window.location.origin + window.location.pathname + "?data=" + new SaveHandler().createSaveData();
+			$('#shareLink').attr("href", shareUrl).html(shareUrl);
+			$('#shareBox').modal();
+		})
+		populateLoadSelect();
 	}
 }); 
 
-function supports_html5_storage() {
-  try {
-    return 'localStorage' in window && window['localStorage'] !== null;
-  } catch (e) {
-    return false;
-  }
+function getUrlParams() {
+  var params = {};
+  window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(str,key,value) {
+    params[key] = value;
+  });
+ 
+  return params;
 }
---
+++ trunk/webaudiotoy/js/saveHandler.js
@@ -0,0 +1,109 @@
+var SaveHandler = Class.extend({
+	init: function(){
+		this.localStorageSavePrefix = "save_";
+	},
+	createSaveData: function() {
+		var save = {
+			nodes: []
+		};
+
+		for(var i in nodes) {
+			var n = nodes[i];
+			if(!n.deleted) {
+				var conns = new Array();
+				for(var j in n.myConnections) {
+					if(!n.myConnections[j].deleted) {
+						conns.push(n.myConnections[j].idx);
+					}
+				}
+
+				save.nodes.push({
+					i: n.idx,
+					sn: n.shortName,
+					p: n.el.offset(),
+					c: conns
+				});
+			}
+		}
+
+		return window.btoa(JSON.stringify(save));
+	},
+	loadSaveData: function(data) {
+		//parse data
+		var save = JSON.parse(window.atob(data));
+
+		//clean up old nodes
+		for(var i in nodes) {
+			for(var j in nodes[i].myConnections) {
+				nodes[i].disconnectFrom(nodes[i].myConnections[j]);
+			}
+			nodes[i].shutdown();
+			nodes[i].el.remove();
+		}
+		$('.line').remove();
+		nodes = new Array();
+
+		//create saved nodes
+		for(var i in save.nodes) {
+			var n = save.nodes[i];
+			var node = null;
+			switch(n.sn) {
+				case 'mn': node = new MicrophoneNode(n.i);	break;
+				case 'gn': node = new GainNode(n.i);	break;
+				case 'scn': node = new ScriptNode(n.i);	break;
+				case 'son': node = new SourceNode(n.i);	break;
+				case 'bfn': node = new BiquadFilterNode(n.i);	break;
+				case 'cn': node = new ConvolverNode(n.i);	break;
+				case 'deln': node = new DelayNode(n.i);	break;
+				case 'dstn': node = new DestinationNode(n.i);	break;
+				case 'dcn': node = new DynamicsCompressorNode(n.i);	break;
+				case 'wsn': node = new WaveShaperNode(n.i);	break;
+				case 'on': node = new OscillatorNode(n.i);	break;
+			}
+			node.el.offset(n.p);
+			nodes[n.i] = node;
+		}
+
+		//connect saved nodes
+		for(var i in save.nodes) {
+			var n = save.nodes[i];
+			if(n.c.length > 0) {
+				for(var j in n.c) {
+					var connectTo = n.c[j];
+					nodes[n.i].connectTo(nodes[connectTo]);
+				}
+			}
+		}
+
+		//create connection lines
+		for(var i in nodes) {
+			var fromN = nodes[i];
+			if(fromN) {
+				for(var j in fromN.myConnections) {
+					var toN = fromN.myConnections[j];
+					fromN.createConnectionLine(fromN.el,toN.el,fromN.idx,toN.idx, false);
+				}
+				fromN.updateConnectionLines();
+			}
+		}
+	},
+	saveToLocalStorage: function(saveName) {
+		var data = this.createSaveData();
+		localStorage[this.localStorageSavePrefix + saveName] = data;
+	}, 
+	loadFromLocalStorage: function(saveName) {
+		var data = localStorage[this.localStorageSavePrefix + saveName];
+		this.loadSaveData(data);
+	},
+	getAllSavesInLocalStorage: function() {
+		var saves = new Array();
+		for (var i = 0; i < localStorage.length; i++) {  
+	        var key = localStorage.key(i);
+	        if(key.substr(0,5) === this.localStorageSavePrefix) {
+	        	saves.push(key.substr(5));
+	        }
+	    }
+		return saves;
+	}
+	 
+});
