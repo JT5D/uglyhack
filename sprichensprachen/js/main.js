dac601276be2c071131920e5a186cf1595684c49
daniel.g.pettersson@gmail.com <daniel.g.pettersson@gmail.com@1e7fd97d-89db-86ba-5077-f0dc3e5da592>
2012-09-28 18:01:23 +0000

    fixing stuff

--- sprichensprachen/js/main.js
+++ sprichensprachen/js/main.js
@@ -12,22 +12,27 @@ $(function() {
 	if(sr != null) {	
 		sr.continuous = true;
 	    sr.onresult = function(event) {
-	      console.log('onresult');
-	      console.log(event);
-	      var sp = $('#speakP');
-	      
-	      var tr = event.result[0].transcript;
 	      var con = event.result[0].confidence;
-	      var span = $('<span>').html(tr).css('font-size', ((con+1.0)*100) + '%');
-	      
-	      sp.append(span);
+	      console.log(con);
+	      if(con > 0.1) {
+		      var sp = $('#speakP');
+		      
+		      var tr = event.result[0].transcript;
+		      
+		      var span = $('<span>').html(tr).css('font-size', ((con+1.0)*100) + '%');
+		      
+		      sp.append(span);
+		  } 
 	    };
 	    
 	    sr.onend = function(event) { 
-	    	console.log('onend'); 
 	    	sr.start();
 	    };
-	    
+
+	    sr.onstart = function(event) {
+	    	console.log('start');
+	    };
+ 	    
 		sr.start();
 	} else {
 		$('#speakP').append($('<span>').html('You do not seem to have any Javascript Speech API enabled. Try using Chrome Canary and enable Speech JavaScript API in chrome://flags'))
