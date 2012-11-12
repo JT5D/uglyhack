08697a8d79fba009b95e351409df5b922c73812a
daniel.g.pettersson@gmail.com <daniel.g.pettersson@gmail.com@1e7fd97d-89db-86ba-5077-f0dc3e5da592>
2012-11-12 23:04:44 +0000

    did more work on sprichensprachen

--- trunk/sprichensprachen/js/main.js
+++ trunk/sprichensprachen/js/main.js
@@ -1,5 +1,14 @@
 $(function() {
-	
+
+	var targetLang = 'en';
+
+	$('#langselect').change(function() {
+		console.log($('#langselect').val());
+		targetLang = $('#langselect').val();
+	})
+
+	$('#browserLang').html('Your browser language is ' + navigator.language);
+
 	var sr = null;
 	if(typeof (SpeechRecognition) === 'function') {
 		sr = new SpeechRecognition();
@@ -12,18 +21,40 @@ $(function() {
 	if(sr != null) {	
 		sr.continuous = true;
 	    sr.onresult = function(event) {
-	      var con = event.result[0].confidence;
-	      console.log(con);
-	      if(con > 0.1) {
-		      var sp = $('#speakP');
-		      
-		      var tr = event.result[0].transcript;
-		      
-		      var span = $('<span>').html(tr).css('font-size', ((con+1.0)*100) + '%');
-		      
-		      sp.append(span);
-		  } 
-	    };
+		    var con = event.result[0].confidence;
+		    var sp = $('#speakP');
+		    var tp = $('#transP').html('');
+
+		    if(con > 0.2) {
+			      
+		      	var tr = event.result[0].transcript;
+		      	sp.html($('<span>').html(tr).css('font-size', ((con+1.0)*100) + '%'));
+
+		      	$.getJSON('https://www.googleapis.com/language/translate/v2?key=AIzaSyBFbIbIAybTBKW_HWtLWtS6IDSPf-gK5Uk&source=' + 
+					navigator.language + '&target=' + targetLang + '&q=' + encodeURIComponent(tr) + '&callback=?', function(data) {
+
+					var transText = $("<div/>").html(data.data.translations[0].translatedText).text();
+					console.log(transText);
+					tp.html($('<span>').html(transText));
+
+					var as = document.getElementById("audioSrc");
+					as.src = 'http://translate.google.com/translate_tts?tl=' + targetLang + '&q=' + transText.replace(/ /g, '+');
+					as.load();
+					as.play();
+
+	  			}).error(function() {
+	  				var span = $('<span>').html('Could not translate ' + tr);
+				  	span.css('color', 'red');
+				    sp.html(span);
+	  			});
+
+		    } else {
+		   		var span = $('<span>').html('I didn\'t get that.. Confidence was ' + Math.floor(con*100) + '%');
+			  	span.css('color', 'red');
+			    sp.html(span);
+			}
+
+		};
 	    
 	    sr.onend = function(event) { 
 	    	sr.start();
