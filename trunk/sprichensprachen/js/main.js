0a782b0122b548205c8033aecb83e9936ae8b51d
daniel.g.pettersson@gmail.com <daniel.g.pettersson@gmail.com@1e7fd97d-89db-86ba-5077-f0dc3e5da592>
2012-11-13 08:39:26 +0000

    fixed playing of translated speeach using ugly iframe.

--- trunk/sprichensprachen/js/main.js
+++ trunk/sprichensprachen/js/main.js
@@ -1,14 +1,36 @@
 $(function() {
 
-	var targetLang = 'en';
+	var targetLang = 'fr';
 
 	$('#langselect').change(function() {
 		console.log($('#langselect').val());
 		targetLang = $('#langselect').val();
 	})
-
+	
 	$('#browserLang').html('Your browser language is ' + navigator.language);
+	
+	var translateAndSpeak = function(speakStr, con) {
+		$('#speakP').html($('<span>').html(speakStr).css('font-size', ((con+1.0)*100) + '%'));
 
+      	$.getJSON('https://www.googleapis.com/language/translate/v2?key=AIzaSyBFbIbIAybTBKW_HWtLWtS6IDSPf-gK5Uk&source=' + 
+			navigator.language.substr(0,2) + '&target=' + targetLang + '&q=' + encodeURIComponent(speakStr) + '&callback=?', function(data) {
+
+			var transText = $("<div/>").html(data.data.translations[0].translatedText).text();
+			$('#transP').html($('<span>').html(transText));
+
+			var ifrm = document.createElement("IFRAME"); 
+		    ifrm.setAttribute("src", "http://translate.google.com/translate_tts?tl=" + targetLang + "&q=" + transText.replace(/ /g, '+')); 
+		    ifrm.style.width = 0+"px"; 
+		    ifrm.style.height = 0 +"px"; 
+		    document.body.appendChild(ifrm); 
+
+			}).error(function() {
+				var span = $('<span>').html('Could not translate ' + tr);
+		  	span.css('color', 'red');
+		  	$('#speakP').html(span);
+		});
+	} ;
+	
 	var sr = null;
 	if(typeof (SpeechRecognition) === 'function') {
 		sr = new SpeechRecognition();
@@ -22,36 +44,16 @@ $(function() {
 		sr.continuous = true;
 	    sr.onresult = function(event) {
 		    var con = event.result[0].confidence;
-		    var sp = $('#speakP');
-		    var tp = $('#transP').html('');
+		    $('#transP').html('');
 
 		    if(con > 0.2) {
-			      
 		      	var tr = event.result[0].transcript;
-		      	sp.html($('<span>').html(tr).css('font-size', ((con+1.0)*100) + '%'));
-
-		      	$.getJSON('https://www.googleapis.com/language/translate/v2?key=AIzaSyBFbIbIAybTBKW_HWtLWtS6IDSPf-gK5Uk&source=' + 
-					navigator.language + '&target=' + targetLang + '&q=' + encodeURIComponent(tr) + '&callback=?', function(data) {
-
-					var transText = $("<div/>").html(data.data.translations[0].translatedText).text();
-					console.log(transText);
-					tp.html($('<span>').html(transText));
-
-					var as = document.getElementById("audioSrc");
-					as.src = 'http://translate.google.com/translate_tts?tl=' + targetLang + '&q=' + transText.replace(/ /g, '+');
-					as.load();
-					as.play();
-
-	  			}).error(function() {
-	  				var span = $('<span>').html('Could not translate ' + tr);
-				  	span.css('color', 'red');
-				    sp.html(span);
-	  			});
+		      	translateAndSpeak(tr, con);
 
 		    } else {
 		   		var span = $('<span>').html('I didn\'t get that.. Confidence was ' + Math.floor(con*100) + '%');
 			  	span.css('color', 'red');
-			    sp.html(span);
+			  	$('#speakP').html(span);
 			}
 
 		};
