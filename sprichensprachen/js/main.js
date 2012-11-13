b416ad6b368e1dcfb47e621dc4d439472b9cedb1
daniel.g.pettersson@gmail.com <daniel.g.pettersson@gmail.com@1e7fd97d-89db-86ba-5077-f0dc3e5da592>
2012-11-13 23:57:47 +0000

    first complete sprichensprachen

--- sprichensprachen/js/main.js
+++ sprichensprachen/js/main.js
@@ -1,31 +1,92 @@
+var targetLang = 'es';
+var confidence = 0.4;
+
+
+function setTargetLang(el) {
+	targetLang = el.href.substr(el.href.length-2, 2);
+	$('#transBtn').html(el.text);
+	return false;
+}
+
+var gApiKey = 'AIzaSyBFbIbIAybTBKW_HWtLWtS6IDSPf-gK5Uk';
+
 $(function() {
 
-	var targetLang = 'fr';
 
-	$('#langselect').change(function() {
-		console.log($('#langselect').val());
-		targetLang = $('#langselect').val();
+	$('#confidenceRange').slider({
+		min: 0.01,
+		max: 0.99,
+		step: 0.01,
+		value: confidence,
+		slide: function(el, v) { 
+			confidence = v.value;
+			$('#confidenceLabel').html('Confidence ' + Math.floor(v.value * 100) + '%');
+		}
+	});
+
+	$('.toool').tooltip({
+		placement: 'bottom'
+	});
+
+	$('#firstTimeChk').on('change', function() {
+		if(this.checked) {
+			localStorage["beenherebefore"] = 'yes';
+		} else {
+			localStorage.removeItem("beenherebefore");
+		}
+	});
+
+	if(!localStorage["beenherebefore"]) {
+		localStorage["beenherebefore"] = 'yes';
+		$('#firstTimeBox').modal();
+	}
+
+	$.getJSON('https://www.googleapis.com/language/translate/v2/languages?key=' + gApiKey + '&target=en&callback=?', function(data) {
+		var bl = navigator.language.substr(0,2);
+		for(var i in data.data.languages) {
+			var l = data.data.languages[i];
+		    if(l.language == bl) {
+		        bl = l.name;
+		    }
+		    $('#transDropMenu').append($('<li><a href="#' + l.language + '" onclick="return setTargetLang(this);">' + l.name + '</a></li>'));
+		}
+		$('#browserLang').html('You should speak in ' + bl);
 	})
-	
-	$('#browserLang').html('Your browser language is ' + navigator.language);
-	
-	var translateAndSpeak = function(speakStr, con) {
-		$('#speakP').html($('<span>').html(speakStr).css('font-size', ((con+1.0)*100) + '%'));
 
-      	$.getJSON('https://www.googleapis.com/language/translate/v2?key=AIzaSyBFbIbIAybTBKW_HWtLWtS6IDSPf-gK5Uk&source=' + 
+	var iframeSpeak = function(transText) {
+		var ifrm = document.createElement("IFRAME"); 
+	    ifrm.setAttribute("src", "http://translate.google.com/translate_tts?tl=" + targetLang + "&q=" + transText.replace(/ /g, '+')); 
+	    ifrm.style.width = 0+"px"; 
+	    ifrm.style.height = 0 +"px"; 
+	    document.body.appendChild(ifrm); 
+	};
+
+	var translateAndSpeak = function(speakStr, con) {
+		$('#speakP').html($('<span>').html(speakStr));
+
+		if(targetLang == navigator.language.substr(0,2)) {
+			$('#transP').html($('<span>').html(speakStr));
+			iframeSpeak(speakStr);
+			return;
+		}
+
+      	$.getJSON('https://www.googleapis.com/language/translate/v2?key=' + gApiKey + '&source=' + 
 			navigator.language.substr(0,2) + '&target=' + targetLang + '&q=' + encodeURIComponent(speakStr) + '&callback=?', function(data) {
+
+			if(data.error) {
+				var span = $('<span>').html(data.error.message);
+			  	span.css('color', 'red');
+			  	$('#speakP').html(span);
+			  	return;
+			}
 
 			var transText = $("<div/>").html(data.data.translations[0].translatedText).text();
 			$('#transP').html($('<span>').html(transText));
 
-			var ifrm = document.createElement("IFRAME"); 
-		    ifrm.setAttribute("src", "http://translate.google.com/translate_tts?tl=" + targetLang + "&q=" + transText.replace(/ /g, '+')); 
-		    ifrm.style.width = 0+"px"; 
-		    ifrm.style.height = 0 +"px"; 
-		    document.body.appendChild(ifrm); 
+			iframeSpeak(transText);
 
-			}).error(function() {
-				var span = $('<span>').html('Could not translate ' + tr);
+		}).error(function() {
+			var span = $('<span>').html('Could not translate ' + speakStr);
 		  	span.css('color', 'red');
 		  	$('#speakP').html(span);
 		});
@@ -46,12 +107,13 @@ $(function() {
 		    var con = event.result[0].confidence;
 		    $('#transP').html('');
 
-		    if(con > 0.2) {
+		    if(con > confidence) {
 		      	var tr = event.result[0].transcript;
 		      	translateAndSpeak(tr, con);
+		      	$('#whatyousaid').html('What you said with ' + Math.floor(con*100) + '% confidence')
 
 		    } else {
-		   		var span = $('<span>').html('I didn\'t get that.. Confidence was ' + Math.floor(con*100) + '%');
+		   		var span = $('<span>').html('I\'m not sure I heard you right. Confidence was ' + Math.floor(con*100) + '%');
 			  	span.css('color', 'red');
 			  	$('#speakP').html(span);
 			}
