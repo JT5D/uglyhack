51fa013f0e5143d5f002d33df8abfd12db91a9b0
daniel.g.pettersson@gmail.com <daniel.g.pettersson@gmail.com@1e7fd97d-89db-86ba-5077-f0dc3e5da592>
2014-01-21 22:18:51 +0000

    added some more monsters

--- td2/src/main.js
+++ td2/src/main.js
@@ -1,4 +1,7 @@
-var TD = {};
+var TD = {
+    pixiStage: null,
+    monsters: []
+};
 
 TD.Displayable = Class.extend({
   init: function(parent, textureSrc, pos){
@@ -12,35 +15,42 @@ TD.Displayable = Class.extend({
   }
 });
 
-var pixiStage;
-var monsters = [];
-
 $(function() {
     var renderer = new PIXI.WebGLRenderer(1000, 800);
     document.body.appendChild(renderer.view);
-    pixiStage = new PIXI.Stage;
+    TD.pixiStage = new PIXI.Stage;
 
     var map1 = new TD.Map1();
     
     setInterval(function() {
-        setTimeout(function() { monsters.push(new TD.Dragon(map1.sprite, map1.paths[0])); }, Math.random() * 1000);
-        setTimeout(function() { monsters.push(new TD.Wolf(map1.sprite, map1.paths[1])); }, Math.random() * 1000);
-        setTimeout(function() { monsters.push(new TD.Troll(map1.sprite, map1.paths[0])); }, Math.random() * 1000);
-        setTimeout(function() { monsters.push(new TD.Swordman(map1.sprite, map1.paths[1])); }, Math.random() * 1000);
-        setTimeout(function() { monsters.push(new TD.Spider(map1.sprite, map1.paths[0])); }, Math.random() * 1000);
-        setTimeout(function() { monsters.push(new TD.OctoDragon(map1.sprite, map1.paths[0])); }, Math.random() * 1000);
         
-    }, 3000);
+        
+        if (Math.random() < 0.1) TD.monsters.push(new TD.Dragon(map1.sprite, map1.paths[getRandomInt(0, map1.paths.length-1)]));
+        if (Math.random() < 0.1) TD.monsters.push(new TD.Wolf(map1.sprite, map1.paths[getRandomInt(0, map1.paths.length-1)]));
+        if (Math.random() < 0.1) TD.monsters.push(new TD.Troll(map1.sprite, map1.paths[getRandomInt(0, map1.paths.length-1)]));
+        if (Math.random() < 0.1) TD.monsters.push(new TD.Swordman(map1.sprite, map1.paths[getRandomInt(0, map1.paths.length-1)]));
+        if (Math.random() < 0.1) TD.monsters.push(new TD.Spider(map1.sprite, map1.paths[getRandomInt(0, map1.paths.length-1)]));
+        if (Math.random() < 0.1) TD.monsters.push(new TD.OctoDragon(map1.sprite, map1.paths[getRandomInt(0, map1.paths.length-1)]));
+        if (Math.random() < 0.1) TD.monsters.push(new TD.Bat(map1.sprite, map1.paths[getRandomInt(0, map1.paths.length-1)]));
+        if (Math.random() < 0.1) TD.monsters.push(new TD.Bird(map1.sprite, map1.paths[getRandomInt(0, map1.paths.length-1)]));
+        if (Math.random() < 0.1) TD.monsters.push(new TD.IceTroll(map1.sprite, map1.paths[getRandomInt(0, map1.paths.length-1)]));
+        if (Math.random() < 0.1) TD.monsters.push(new TD.Rat(map1.sprite, map1.paths[getRandomInt(0, map1.paths.length-1)]));
+        
+    }, 500);
 
     requestAnimationFrame(animate);
     function animate() {
 
-        renderer.render(pixiStage);
+        renderer.render(TD.pixiStage);
 
-        for(var i in monsters) {
-            monsters[i].updatePosition();
+        for(var i in TD.monsters) {
+            TD.monsters[i].updatePosition();
         }
 
         requestAnimationFrame(animate);
     }
 });
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
