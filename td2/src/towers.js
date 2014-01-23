f240794a0c11979b1f6946b944ccb64204838643
daniel.g.pettersson@gmail.com <daniel.g.pettersson@gmail.com@1e7fd97d-89db-86ba-5077-f0dc3e5da592>
2014-01-23 21:34:58 +0000

    shooting

---
+++ td2/src/towers.js
@@ -0,0 +1,25 @@
+TD.Tower = TD.Displayable.extend({
+	init: function(parent, textureSrc, pos, bulletName, bulletOffset, shotDelay, range) {
+		this._super(parent, textureSrc, pos);
+		this.range = range;
+		this.shotDelay = shotDelay;
+		this.bullets = [];
+		this.bulletName = bulletName;
+		
+		var that = this;
+
+		setInterval(function() {
+
+			var target = TD.monsters[Math.floor(Math.random() * TD.monsters.length)];
+			TD.createBulletInstance(that.bulletName, that.parent, {x: that.sprite.position.x + bulletOffset.x, y: that.sprite.position.y + bulletOffset.y}, target);
+
+		}, this.shotDelay + (Math.random()*100));
+	},
+
+});
+
+TD.Tower1 = TD.Tower.extend({
+	init: function(parent, pos) {
+		this._super(parent, 'img/towers/tower1.png', pos, 'CannonBall', {x: 10, y: 10}, 1000, 200);
+	}
+});
