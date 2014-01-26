d4d4c2a64625dbc14ebe268f1977a1f9025dc720
daniel.g.pettersson@gmail.com <daniel.g.pettersson@gmail.com@1e7fd97d-89db-86ba-5077-f0dc3e5da592>
2014-01-26 13:38:33 +0000

    stuff

--- trunk/td2/src/monsters.js
+++ trunk/td2/src/monsters.js
@@ -17,7 +17,7 @@ TD.Monster = TD.Displayable.extend({
     this.dying = false;
 
     var that = this;
-    this.tween = TweenMax.to(this.pos, path.len / 200, {
+    this.tween = TweenMax.to(this.pos, path.len / 100, {
       bezier: {'values': path.path},
       ease: 'Linear.easeNone',
       onComplete: function() {
@@ -45,25 +45,25 @@ TD.Monster = TD.Displayable.extend({
 
 TD.Dragon = TD.Monster.extend({
   init: function(parent, path) {
-    this._super(parent, 'img/monsters/dragon/small.png', path, 0.7);
+    this._super(parent, 'img/monsters/dragon/small.png', path, 0.71);
   }
 });
 
 TD.OctoDragon = TD.Monster.extend({
   init: function(parent, path) {
-    this._super(parent, 'img/monsters/octodragon/small.png', path, 1.0);
+    this._super(parent, 'img/monsters/octodragon/small.png', path, 1.02);
   }
 });
 
 TD.Wolf = TD.Monster.extend({
   init: function(parent, path) {
-    this._super(parent, 'img/monsters/wolf/small.png', path, 1.3);
+    this._super(parent, 'img/monsters/wolf/small.png', path, 1.33);
   }
 });
 
 TD.Troll = TD.Monster.extend({
   init: function(parent, path) {
-    this._super(parent, 'img/monsters/troll/small.png', path, 0.6);
+    this._super(parent, 'img/monsters/troll/small.png', path, 0.64);
   }
 });
 
@@ -75,42 +75,42 @@ TD.Swordman = TD.Monster.extend({
 
 TD.Spider = TD.Monster.extend({
   init: function(parent, path) {
-    this._super(parent, 'img/monsters/spider/small.png', path, 1.0);
+    this._super(parent, 'img/monsters/spider/small.png', path, 1.06);
   }
 });
 
 TD.Bat = TD.Monster.extend({
   init: function(parent, path) {
-    this._super(parent, 'img/monsters/bat/small.png', path, 0.8);
+    this._super(parent, 'img/monsters/bat/small.png', path, 0.87);
   }
 });
 
 TD.Bird = TD.Monster.extend({
   init: function(parent, path) {
-    this._super(parent, 'img/monsters/bird/small.png', path, 0.6);
+    this._super(parent, 'img/monsters/bird/small.png', path, 0.68);
   }
 });
 
 TD.IceTroll = TD.Monster.extend({
   init: function(parent, path) {
-    this._super(parent, 'img/monsters/icetroll/small.png', path, 0.5);
+    this._super(parent, 'img/monsters/icetroll/small.png', path, 0.59);
   }
 });
 
 TD.Rat = TD.Monster.extend({
   init: function(parent, path) {
-    this._super(parent, 'img/monsters/rat/small.png', path, 0.85);
+    this._super(parent, 'img/monsters/rat/small.png', path, 0.86);
   }
 });
 
 TD.Sheep = TD.Monster.extend({
   init: function(parent, path) {
-    this._super(parent, 'img/monsters/sheep/small.png', path, 0.65);
+    this._super(parent, 'img/monsters/sheep/small.png', path, 0.61);
   }
 });
 
 TD.Fireman = TD.Monster.extend({
   init: function(parent, path) {
-    this._super(parent, 'img/monsters/fireman/small.png', path, 0.8);
+    this._super(parent, 'img/monsters/fireman/small.png', path, 0.82);
   }
 });
